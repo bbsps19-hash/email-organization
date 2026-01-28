d151364a8c38ad843331394198e24fb20eda450c
genious467 <genious467@gmail.com>
2026-01-28 11:59:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -322,7 +322,8 @@ const decodeMimeWords = (value) => {
     }
     return pickBestDecoded(bytes, charset);
   });
-  return decoded.replace(/=\?=\s*/g, '').replace(/\s{2,}/g, ' ').trim();
+  const cleaned = decoded.replace(/=\?=\s*/g, '').replace(/\s{2,}/g, ' ').trim();
+  return repairMojibake(cleaned);
 };
 
 const decodeAttachmentName = (value) => {
@@ -459,45 +460,8 @@ const latin1FromBuffer = (buffer) =>
 const latin1ToBytes = (text) => Uint8Array.from(text, (ch) => ch.charCodeAt(0));
 
 const windows1252ToBytes = (text) => {
-  const map = {
-    0x20ac: 0x80,
-    0x201a: 0x82,
-    0x0192: 0x83,
-    0x201e: 0x84,
-    0x2026: 0x85,
-    0x2020: 0x86,
-    0x2021: 0x87,
-    0x02c6: 0x88,
-    0x2030: 0x89,
-    0x0160: 0x8a,
-    0x2039: 0x8b,
-    0x0152: 0x8c,
-    0x017d: 0x8e,
-    0x2018: 0x91,
-    0x2019: 0x92,
-    0x201c: 0x93,
-    0x201d: 0x94,
-    0x2022: 0x95,
-    0x2013: 0x96,
-    0x2014: 0x97,
-    0x02dc: 0x98,
-    0x2122: 0x99,
-    0x0161: 0x9a,
-    0x203a: 0x9b,
-    0x0153: 0x9c,
-    0x017e: 0x9e,
-    0x0178: 0x9f,
-  };
-  const bytes = new Uint8Array(text.length);
-  for (let i = 0; i < text.length; i += 1) {
-    const code = text.charCodeAt(i);
-    if (code <= 0xff) {
-      bytes[i] = code;
-      continue;
-    }
-    bytes[i] = map[code] ?? 0x3f;
-  }
-  return bytes;
+  if (!text) return new Uint8Array(0);
+  return Uint8Array.from(text, (ch) => ch.charCodeAt(0));
 };
 
 const percentToBytes = (value) => {
